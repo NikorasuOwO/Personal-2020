e2cf81e24c7d42bc86c72277dfab62d755c913c9
NikorasuOwO <>
2021-09-20 14:31:28 +0200

    filtro empezado

--- eCommerce/eCommerce/Store/forms.py
+++ eCommerce/eCommerce/Store/forms.py
@@ -2,7 +2,36 @@ from django import forms
 
 class FilterForm(forms.Form):
 
-    usage = forms.TypedChoiceField(label="Usage")
-    color = forms.TypedChoiceField(label="Contains color")
+    order_tuple = [
+    ('Alphabetically', (
+            ('aa', 'Ascending'),
+            ('ad', 'Decending'),
+        )
+    ),
+    ('Numerically', (
+            ('na', 'Ascending'),
+            ('nd', 'Decending'),
+        )
+    )
+    ]
+
+    usage_tuple = [
+
+        ('running','Running'),
+        ('hiking', 'Hiking')
+    ]
+
+    color_tuple = [
+        ('black','Black'),
+        ('red', 'Red'),
+        ('violet','Violet'),
+        ('brown', 'Brown'),
+        ('gray', 'Gray'),
+        ('white', 'White'),
+        ('celeste', 'Celeste')
+    ]
+
+    usage = forms.ChoiceField(label="Usage", choices=usage_tuple)
+    color = forms.ChoiceField(label="Contains color", choices=color_tuple)
     price_below = forms.FloatField(label="Price is below")
-    order = forms.TypedChoiceField(label="Order by")
+    order = forms.ChoiceField(label="Order by", choices=order_tuple)
--- eCommerce/eCommerce/Store/views.py
+++ eCommerce/eCommerce/Store/views.py
@@ -1,7 +1,7 @@
 from Store.models import Product
-from django.shortcuts import get_object_or_404, redirect, render
+from django.shortcuts import get_object_or_404, redirect, render, HttpResponse
 from . import forms
-
+from django.db.models import Q 
 # Create your views here.
 
 def redirect_store(request, *args, **kargs):
@@ -9,11 +9,26 @@ def redirect_store(request, *args, **kargs):
 
 def store(request):
 
-    products = Product.objects.all()
+    if request.method == 'POST':
+
+        # ORDER
+        if request.POST.get('order')[0] == 'n': # NUMERICALLY
+            if request.POST.get('order')[1] == 'a':
+                products = Product.objects.order_by('price')
+            else: products = Product.objects.order_by('-price')
+        else:
+            if request.POST.get('order')[1] == 'a': # ALPHABETICALLY
+                products = Product.objects.order_by('name')
+            else: products = Product.objects.order_by('-name')
+
+        #CONTAINS COLOR
+        products = Product.objects.filter(color1=request.POST.get('color')).union(Product.objects.filter(color2=request.POST.get('color')))
+        filter_form = forms.FilterForm(request.POST)
+    else:
+        products = Product.objects.order_by('name')
+        filter_form = forms.FilterForm()
 
     context = {"products":products, "number":products.count()}
-
-    filter_form = forms.FilterForm()
 
     context["filter_form"] = filter_form
 
